c78c295eea28f5c617ba427b5441e9857506d586
Elizabeth Eidelson <fluturecode@gmail.com>
2022-07-28 22:27:30 -0400

    added wallets and adapters

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,22 @@
 import React, { useMemo } from "react";
 import "../styles/globals.css";
 import "../styles/App.css";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { clusterApiUrl } from "@solana/web3.js";
+import { GlowWalletAdapter, PhantomWalletAdapter, SlopeWalletAdapter, SolflareWalletAdapter, TorusWalletAdapter } from "@solana/wallet-adapter-wallets";
 
 const App = ({ Component, pageProps }) => {
+  const network = WalletAdapterNetwork.Devnet;
+
+  const endpoint = useMemo(() => clusterApiUrl(nework), [network]);
+
+  const wallets = useMem( () => [
+    new PhantomWalletAdapter(),
+    new GlowWalletAdapter(),
+    new SlopeWalletAdapter(),
+    new SolflareWalletAdapter({ network}),
+    new TorusWalletAdapter(),
+  ], [network]);
 
   return (
     <Component {...pageProps} />
