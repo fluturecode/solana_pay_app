2c0ad1b286e4ece7e81e5fd9fc59a107120c27d0
Elizabeth Eidelson <fluturecode@gmail.com>
2022-07-29 22:17:37 -0400

    added useIPFS hook

--- hooks/useIPFS.js
+++ hooks/useIPFS.js
@@ -1,5 +1,19 @@
-const useIPFS = (hash, filename) => {
-return `https://gateway.ipfscdn.io/ipfs/${hash}?filename=${filename}`
+
+const IPFSDownload = ({hash, filename}) => {
+
+const file = useIPFS(hash, filename);
+
+return (
+    <div>
+      {file ? (
+        <div className='download-component'>
+          <a className='download-button' href={file} download={filename}>Download</a>
+        </div>
+      ) : (
+        <p>Downloading file...</p>
+      )}
+    </div>
+  )
 };
 
-export default useIPFS;
+export default IPFSDownload;
