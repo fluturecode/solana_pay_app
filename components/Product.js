0b0d6b1e3620036480cecc8d8934571b85973c78
Elizabeth Eidelson <fluturecode@gmail.com>
2022-08-01 22:12:09 -0400

    added ipfs files to product.js

--- components/Product.js
+++ components/Product.js
@@ -10,17 +10,14 @@ export default function Product({ product }) {
       <div >
         <img className={styles.product_image}src={image_url} alt={name} />
       </div>
-
       <div className={styles.product_details}>
         <div className={styles.product_text}>
           <div className={styles.product_title}>{name}</div>
           <div className={styles.product_description}>{description}</div>
         </div>
-
         <div className={styles.product_action}>
           <div className={styles.product_price}>{price} USDC</div>
-          {/* I'm hardcoding these for now, we'll fetch the hash from the API later*/}
-          <IPFSDownload filename="emojis.zip" hash="QmWWH69mTL66r3H8P4wUn24t1L5pvdTJGUTKBqT11KCHS5" cta="Download emojis"/>
+          <IPFSDownload filename="emojis.zip" hash="https://cloudflare-ipfs.com/ipfs/Qmf16QwaKEbmAXnnZYGqaCpVNQFWEdVG8nAwwJfExs6E4S/beastie_album.jpeg" cta="Download emojis"/>
         </div>
       </div>
     </div>
